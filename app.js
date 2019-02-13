87fb2a44b33fa81a92cade32ee1e80c85f0c9fe9
KhallilB <khallil.bailey@gmail.com>
2019-02-13 13:14:34 -0800

    logic for MVP

--- app.js
+++ app.js
@@ -2,4 +2,14 @@ var budgetController = (function() {})();
 
 var UIController = (function() {})();
 
-var controller = (function() {})();
+var controller = (function(bugetCtrl, UICtrl) {
+  document.querySelector('.add-button').addEventListener('click', function() {
+    console.log('button clicked');
+    //TODO:
+    // get input data,
+    //add item to budget controller,
+    // add new item to user,
+    //calculate budget,
+    //display the budget
+  });
+})(budgetController, UIController);
