a869d16c30486b12978a71d4f83ca5fc19b76ec2
KhallilB <khallil.bailey@gmail.com>
2019-02-14 12:22:16 -0800

    button function works on click and keypress

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ var budgetController = (function() {})();
 var UIController = (function() {})();
 
 var controller = (function(bugetCtrl, UICtrl) {
-  document.querySelector('.add-button').addEventListener('click', function() {
-    console.log('button clicked');
+  var ctrlAddItem = function() {
     //TODO:
     // get input data,
     //add item to budget controller,
     // add new item to user,
     //calculate budget,
     //display the budget
+    console.log('DagNaBit');
+  };
+
+  document.querySelector('.add-button ').addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function(e) {
+    //Handles enter event when submiting to list
+    if (e.keyCode === 13 || e.which === 13) {
+      //For older browsers who dont have key code property
+      console.log('ENTER was pressed');
+      ctrlAddItem();
+    }
   });
 })(budgetController, UIController);
